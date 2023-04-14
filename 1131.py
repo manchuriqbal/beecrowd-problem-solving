a19e50122cbb7335c6227933ab8dfdda711e4413
manchur <manchur.fiverr@gmail.com>
2023-04-14 17:05:13 +0600

    i try to slove 1131 but can't

---
+++ 1131.py
@@ -0,0 +1,47 @@
+match=0
+inter=0
+gremio=0
+deaw=0
+count=1
+
+while count == 1:
+    try:
+        
+        i, g=map(int, input().split())
+        print("Novo grenal (1-sim 2-nao)")
+        match+=1
+
+        if i > g:
+            inter+=1
+        elif i < g:
+            gremio+=1
+        else:
+            deaw+=1
+
+        while True:
+            
+            n= int(input())
+            
+            if n ==2:
+                count=n
+                break
+            elif n == 1:
+                break
+    
+    except EOFError:
+        break
+if inter > gremio:
+    winner= "Inter venceu mais"
+elif inter < gremio:
+    winner= "Gremio venceu mais"
+else: 
+    winner="Não houve vencedor"
+
+
+print(f"""
+{match} grenais
+Inter:{inter}
+Gremio:{gremio}
+Empates:{deaw}
+{winner}
+""")
