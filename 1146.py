74652d8a47f42edd3de45d8611c1124f15d7d2ef
manchur <manchur.fiverr@gmail.com>
2023-05-22 07:40:37 +0600

    i solved 1146 no problem

--- 1146.py
+++ 1146.py
@@ -1,8 +1,11 @@
 while True:
-
     x = int(input())
-    for i in range(1,x+1):
-        print(i, end=" ")
 
     if x == 0:
         break
+    else:
+        for i in range(1,x):
+            print(i, end=" ")
+    print(x)
+
+    
